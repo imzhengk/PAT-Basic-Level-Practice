db78dff6e666c308459cfffa5d9678987f6a946e
imzhengk <imzhengk@gmail.com>
2018-10-08 22:06:26 +0800

    add show

---
+++ Show/Example11_7.java
@@ -0,0 +1,9 @@
+package Show;
+
+public class Example11_7 {
+   public static void main(String args[]) {
+      WindowItemEvent win=new WindowItemEvent();
+      win.setBounds(100,100,700,500);
+      win.setTitle("处理ItemEvent事件");
+   }
+}
